550332fd0496c41bea190ee4a09c85b3495a3285
lexa5575 <66919275+lexa5575@users.noreply.github.com>
2022-06-27 19:26:36 +0200

    fierst

--- main/routes/web.php
+++ main/routes/web.php
@@ -2,23 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
-
-Route::get('/aboutjjjj', function () {
-    return view('about');
-})->name('about');
-
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
-
-Route::get('/contact/all/{id}/delete', 'App\Http\Controllers\ContactController@deleteMessage')->name('contact-delete');
-Route::get('/contact/all/{id}/update', 'App\Http\Controllers\ContactController@updateMessage')->name('contact-update');
-Route::get('/contact/all/{id}','App\Http\Controllers\ContactController@showOneMessage')->name('contact-data-one');
-Route::get('/contact/all','App\Http\Controllers\ContactController@allData')->name('contact-data');
-Route::post('/contact/submit','App\Http\Controllers\ContactController@submit')->name('contact-form');
-Route::post('/contact/all/{id}/update', 'App\Http\Controllers\ContactController@updateMessageSubmit')->name('contact-update-submit');
-
