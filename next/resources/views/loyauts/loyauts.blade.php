9c135aaab6092154ba3408a0d5c0e3d704024247
lexa5575 <66919275+lexa5575@users.noreply.github.com>
2022-06-28 21:42:40 +0200

    js

--- next/resources/views/loyauts/loyauts.blade.php
+++ next/resources/views/loyauts/loyauts.blade.php
@@ -7,10 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
 @include('inc.header')
 @yield('content')
+
 @include('inc.footer')
+
 </body>
 </html>
